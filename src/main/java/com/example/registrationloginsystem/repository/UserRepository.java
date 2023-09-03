a46544971f9c78470877dc5a167da952c460353e
Alvaro <jlinaresmedalla@gmail.com>
2023-09-02 19:15:36 -0500

    update feature

---
+++ src/main/java/com/example/registrationloginsystem/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.registrationloginsystem.repository;
+
+import com.example.registrationloginsystem.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
